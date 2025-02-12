219a4e4a53f2b47256252919bf0aca1d54ae45c5
Isabella Inglis <issy.inglis@hotmail.co.uk>
2025-02-12 14:34:07 +0000

    add comment card data

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,38 +1,65 @@
-const commentForm = document.querySelector(".comment-section");
+let displayComments = (comment) => {
+  const commentSection = document.querySelector(".comment-section");
 
-const avatar = document.createElement("img");
-avatar.classList.add("comment-section__avatar");
-avatar.setAttribute("src", "../assets/images/mohan-muruge.jpg");
-commentForm.appendChild(avatar);
+  const commentCard = document.createElement("div");
+  commentCard.classList.add("comment-section__comment-card");
+  commentSection.appendChild(commentCard);
 
-const form = document.createElement("form");
-form.classList.add("comment-section__form");
+  const commentImgContainer = document.createElement("div");
+  commentImgContainer.classList.add("comment-section__comment-img-container");
+  commentCard.appendChild(commentImgContainer);
 
-const nameLabel = document.createElement("label");
-nameLabel.classList.add("comment-section__label");
-nameLabel.setAttribute("for", "nameInput");
-nameLabel.innerText = "NAME";
-commentForm.appendChild(nameLabel);
+  const commentImg = document.createElement("img");
+  commentImg.classList.add("comment-section__comment-img");
+  commentImgContainer.appendChild(commentImg);
 
-const nameInput = document.createElement("input");
-nameInput.classList.add("comment-section__name-input");
-nameInput.setAttribute("name", "nameInput");
-nameInput.setAttribute("id", "nameInput");
-commentForm.appendChild(nameInput);
+  const commentContainer = document.createElement("div");
+  commentContainer.classList.add("comment-section__comment-container");
+  commentCard.appendChild(commentContainer);
 
-const commentLabel = document.createElement("label");
-commentLabel.classList.add("comment-section__label");
-commentLabel.setAttribute("for", "commentInput");
-commentLabel.innerText = "COMMENT";
-commentForm.appendChild(commentLabel);
+  const commentTop = document.createElement("div");
+  commentTop.classList.add("comment-section__comment-top");
+  commentContainer.appendChild(commentTop);
 
-const commentInput = document.createElement("input");
-commentInput.classList.add("comment-section__comment-input");
-commentInput.setAttribute("comment", "commentInput");
-commentInput.setAttribute("id", "commentInput");
-commentForm.appendChild(commentInput);
+  const commentName = document.createElement("p");
+  commentName.classList.add("comment-section__comment-name");
+  commentName.innerText = comment.name;
+  commentTop.appendChild(commentName);
 
-const commentBtn = document.createElement("button");
-commentBtn.classList.add("comment-section__btn");
-commentBtn.innerText = "COMMENT";
-commentForm.appendChild(commentBtn);
+  const commentDate = document.createElement("p");
+  commentDate.classList.add("comment-section__comment-Date");
+  commentDate.innerText = comment.date;
+  commentTop.appendChild(commentDate);
+
+  const commentContent = document.createElement("p");
+  commentContent.classList.add("comment-section__comment-content");
+  commentContent.innerText = comment.content;
+  commentContainer.appendChild(commentContent);
+};
+
+const comments = [
+  {
+    name: "Victor Pinto",
+    content:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    date: "01/12/2025",
+  },
+  {
+    name: "Christina Cabrera",
+    content:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    date: "01/02/2025",
+  },
+  {
+    name: "Isaac Tadesse",
+    content:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    date: "12/20/2024",
+  },
+];
+
+for (let i = 0; i < comments.length; i++) {
+  const comment = comments[i];
+
+  displayComments(comment);
+}
