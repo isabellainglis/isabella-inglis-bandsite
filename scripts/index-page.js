5ef4d2cff49db842d620fd390142a4b5d166ab7d
Isabella Inglis <issy.inglis@hotmail.co.uk>
2025-02-18 14:28:39 +0000

    replace comment data with API

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -21,41 +21,18 @@ class BandSiteApi {
 
 const bandsiteApi = new BandSiteApi(apiKey);
 
-const test = async () => {
-  const comments = await bandsiteApi.getComments();
-  console.log(comments);
-};
-
-test();
-
-const comments = [
-  {
-    name: "Victor Pinto",
-    content:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    date: "01/12/2025",
-  },
-  {
-    name: "Christina Cabrera",
-    content:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    date: "01/02/2025",
-  },
-  {
-    name: "Isaac Tadesse",
-    content:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    date: "12/20/2024",
-  },
-];
-
 const commentSection = document.querySelector(
   ".comment-section__comment-cards"
 );
 
-let displayComments = () => {
+const displayComments = async () => {
+  const comments = await bandsiteApi.getComments();
+
   for (let i = 0; i < comments.length; i++) {
     const comment = comments[i];
+    const date = new Date(comment.timestamp).toLocaleDateString();
+    const userName = comment.name;
+    const userComment = comment.comment;
 
     const commentCard = document.createElement("div");
     commentCard.classList.add("comment-section__comment-card");
@@ -79,26 +56,27 @@ let displayComments = () => {
 
     const commentName = document.createElement("p");
     commentName.classList.add("comment-section__comment-name");
-    commentName.innerText = comment.name;
+    commentName.innerText = userName;
     commentTop.appendChild(commentName);
 
     const commentDate = document.createElement("p");
     commentDate.classList.add("comment-section__comment-date");
-    commentDate.innerText = comment.date;
+    commentDate.innerText = date;
     commentTop.appendChild(commentDate);
 
     const commentContent = document.createElement("p");
     commentContent.classList.add("comment-section__comment-content");
-    commentContent.innerText = comment.content;
+    commentContent.innerText = userComment;
     commentContainer.appendChild(commentContent);
   }
 };
 
 const formEl = document.querySelector(".comment-section__form");
 
-formEl.addEventListener("submit", (event) => {
+formEl.addEventListener("submit", async (event) => {
   event.preventDefault();
 
+  const comments = await bandsiteApi.getComments();
   const errorEl = document.querySelector(".comment-section__error");
   const nameInputEl = document.querySelector(".comment-section__input-name");
   const commentInputEl = document.querySelector(
@@ -127,19 +105,7 @@ formEl.addEventListener("submit", (event) => {
     commentInputEl.classList.add("comment-section__input-comment--error");
   } else {
     const currentDate = new Date();
-    let day = currentDate.getDate();
-    let month = currentDate.getMonth();
-    let year = currentDate.getFullYear();
-
-    if (day < 10) {
-      day = "0" + day;
-    }
-
-    if (month < 10) {
-      month = "0" + month;
-    }
-
-    let formattedDate = `${day}/${month}/${year}`;
+    let formattedDate = currentDate.toLocaleDateString();
 
     const newComment = {
       name: event.target.name.value,
