da3c82ba4f993b35202a34b3faa7a42b6a93dec7
Isabella Inglis <issy.inglis@hotmail.co.uk>
2025-02-18 12:09:03 +0000

    link axios

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,3 +1,33 @@
+const apiKey = "d07d1e8d-6301-4bcf-b713-53d5ff9dedc5";
+
+class BandSiteApi {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+  }
+
+  async getComments() {
+    const comments = await axios.get(
+      `${this.baseUrl}comments?api_key=${this.apiKey}`
+    );
+    return comments.data;
+  }
+
+  async postComment(comment) {
+    const newComment = await axios.post(comment);
+    return newComment.data;
+  }
+}
+
+const bandsiteApi = new BandSiteApi(apiKey);
+
+const test = async () => {
+  const comments = await bandsiteApi.getComments();
+  console.log(comments);
+};
+
+test();
+
 const comments = [
   {
     name: "Victor Pinto",
